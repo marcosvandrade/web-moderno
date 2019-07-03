847379000fe65d36be6135ff6a4d6fd1b91e700e
Marcos Vasconcellos de Andrade <dcemvasconcellos@dgp.local>
2019-07-03 16:01:39 -0300

    work3Jul19

---
+++ exercicios-pratica/fundamentos/strings.js
@@ -0,0 +1,9 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charCodeAt(3))
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log(escola.replace(3,'e'))
+
+console.log('Ana,Maria,Pedro'.split(','))
