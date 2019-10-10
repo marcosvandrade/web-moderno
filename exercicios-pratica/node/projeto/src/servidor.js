510cfc14a216836bf03ed23c49642bd7ea4052ae
Marcos Vasconcellos de Andrade <vascorj2006@gmail.com>
2019-10-10 16:51:57 -0300

    work10out19

--- exercicios-pratica/node/projeto/src/servidor.js
+++ exercicios-pratica/node/projeto/src/servidor.js
@@ -2,15 +2,35 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bandoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', (req, res, next) => {
-    console.log('Middleware 1...')
-    next()
-})
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res) => {
-    res.send({ nome: 'Notebook', preco: 123.45}) // Converter para JSON
+    res.send(bandoDeDados.getProdutos())
 })
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bandoDeDados.getProduto(req.params.id)) // Converter para JSON
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bandoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bandoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+}) 
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
