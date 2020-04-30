53e0ef543bdb5b8684e367a365220c5b0ad5b738
marcosvandrade <vascorj2006@gmail.com>
2020-04-30 10:41:40 -0300

    30abr20

--- exercicios-web/node/temporizador.js
+++ exercicios-web/node/temporizador.js
@@ -1,22 +1,22 @@
 const schedule = require('node-schedule')
 
-const tarefa1 = schedule.scheduleJob('*/5 * 12 * * 2', function () {
+const tarefa1 = schedule.scheduleJob('*/5 * 10 * * 4', function () {
     console.log('Executando Tarefa 1!', new Date().getSeconds())
 })
 
-setTimeout(function () {
-    tarefa1.cancel()
-    console.log('Cancelando Tarefa 1!')
-}, 20000)
+ setTimeout(function () {
+     tarefa1.cancel()
+     console.log('Cancelando Tarefa 1!')
+ }, 20000)
 
 // setImmediate
 // setInterval
 
-const regra = new schedule.RecurrenceRule()
-regra.dayOfWeek = [new schedule.Range(1, 5)]
-regra.hour = 12
-regra.second = 30
+ const regra = new schedule.RecurrenceRule()
+ regra.dayOfWeek = [new schedule.Range(1, 5)]
+ regra.hour = 10
+ regra.second = 30
 
-const tarefa2 = schedule.scheduleJob(regra, function () {
-    console.log('Executando Tarefa 2!', new Date().getSeconds())
-})
+ const tarefa2 = schedule.scheduleJob(regra, function () {
+     console.log('Executando Tarefa 2!', new Date().getSeconds())
+ })
