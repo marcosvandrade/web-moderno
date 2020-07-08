57925f7300a83ee940a8dee5394b277d4124e0ab
marcosvandrade <vascorj2006@gmail.com>
2020-07-08 15:09:27 -0300

    home8jul20

--- exercicios-web/ajax/server.js
+++ exercicios-web/ajax/server.js
@@ -2,48 +2,51 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
+// midlewares
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-const multer = require('multer')
+// app.get('/teste', (req, res) => res.send(new Date))
 
-const storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, './upload')
-    },
-    filename: function (req, file, callback) {
-        callback(null, `${Date.now()}_${file.originalname}`)
-    }
-})
+// const multer = require('multer')
 
-const upload = multer({ storage }).single('arquivo')
+// const storage = multer.diskStorage({
+//     destination: function (req, file, callback) {
+//         callback(null, './upload')
+//     },
+//     filename: function (req, file, callback) {
+//         callback(null, `${Date.now()}_${file.originalname}`)
+//     }
+// })
 
-app.post('/upload', (req, res) => {
-    upload(req, res, err => {
-        if (err) {
-            return res.end('Ocorreu um erro.')
-        }
+// const upload = multer({ storage }).single('arquivo')
 
-        res.end('Concluído com sucesso.')
-    })
-})
+// app.post('/upload', (req, res) => {
+//     upload(req, res, err => {
+//         if (err) {
+//             return res.end('Ocorreu um erro.')
+//         }
 
-app.post('/formulario', (req, res) => {
-    res.send({
-        ...req.body,
-        id: 7
-    })
-})
+//         res.end('Concluído com sucesso.')
+//     })
+// })
 
-app.get('/parOuImpar', (req, res) => {
-    // req.body
-    // req.query
-    // req.params
-    const par = parseInt(req.query.numero) % 2 === 0
-    res.send({
-        resultado: par ? 'par' : 'impar'
-    })
-})
+// app.post('/formulario', (req, res) => {
+//     res.send({
+//         ...req.body,
+//         id: 7
+//     })
+// })
+
+// app.get('/parOuImpar', (req, res) => {
+//     // req.body
+//     // req.query
+//     // req.params
+//     const par = parseInt(req.query.numero) % 2 === 0
+//     res.send({
+//         resultado: par ? 'par' : 'impar'
+//     })
+// })
 
 app.listen(8080, () => console.log('Executando...'))
