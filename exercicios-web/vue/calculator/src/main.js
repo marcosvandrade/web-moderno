08b2f0f665a4ef22b018a8da42b9a9e84dd8c9f4
marcosvandrade <vascorj2006@gmail.com>
2020-07-22 11:44:44 -0300

    home22jul20

--- exercicios-web/vue/calculator/src/main.js
+++ exercicios-web/vue/calculator/src/main.js
@@ -4,3 +4,10 @@ import App from './App'
 new Vue({
     render: h => h(App)
 }).$mount("#app")
+
+// new Vue({
+//     el: '#app',
+//     render(createElement) {
+//         return createElement(App)
+//     }
+// })
